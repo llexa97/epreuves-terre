abc6339de5e2489326eabcd8cae895e1e3eedae2
unknown <dondinaxel@icloud.com>
2023-10-10 11:42:52 +0200

    Modification de l'exercice 5 pour inclure les nombres négatifs

--- terre04.py
+++ terre04.py
@@ -27,11 +27,21 @@ if arg == []:
     print("Tu ne me la mettras pas à l’envers.")
 else:
     arg_value = arg[0]
-    
-    if arg_value.isnumeric():
+    # print(arg_value)
+    try:
+        float(arg_value)
         if int(arg_value) % 2 == 0:
             print("pair")
         else:
             print("impair")
-    else: 
+    except:
         print("Tu ne me la mettras pas à l’envers.")
+        
+        
+    # if arg_value.isnumeric():
+    #     if int(arg_value) % 2 == 0:
+    #         print("pair")
+    #     else:
+    #         print("impair")
+    # else: 
+    #     print("Tu ne me la mettras pas à l’envers.")
